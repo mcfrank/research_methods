f20f6ae2656e94ed03c67125acef4ab11e4882eb
Michael C. Frank <mcfrank@stanford.edu>
2018-11-27 16:54:36 -0800

    content pasting

---
+++ _common.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+opts_chunk$set(
+  include = FALSE,
+  message = FALSE,
+  warning = FALSE,
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  echo = FALSE,
+  cache.lazy = FALSE,
+  fig.align = "center",
+  fig.show = "hold"
+)
